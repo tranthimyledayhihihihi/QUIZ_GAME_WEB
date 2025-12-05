a992b89d2cf96de9f8b01dffb99408c14b35ca5c
Thuyhuyenn <nguyenthithuyhuyen905@gmail.com>
2025-12-06 00:28:45 +0700

    Restore full missing folders from branch Quang

---
+++ WEBB/Controllers/Quiz/QuizController.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEBB.Models.Quiz;
+
+namespace WEBB.Controllers.Quiz
+{
+    public class QuizController : Controller
+    {
+        // GET: Quiz
+        public ActionResult Index()
+        {
+            return View(new List<CauHoiDto>());
+        }
+    }
+}
---
+++ WEBB_FE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WEBB_FE.MvcApplication" Language="C#" %>
